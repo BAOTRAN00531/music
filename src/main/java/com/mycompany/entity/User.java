905c80c3872679adcfc6029dd270a5023718c4a5
BAOTRAN00531 <150698874+BAOTRAN00531@users.noreply.github.com>
2024-03-18 22:52:19 +0700

    rename com.mycompany.ui

--- src/main/java/com/mycompany/entity/User.java
+++ src/main/java/com/mycompany/entity/User.java
@@ -9,7 +9,81 @@ package com.mycompany.entity;
  * @author tran
  */
 public class User {
-        private String maNV;
-        private String matKhau;
-        private String hoTen;
+        private String maND;
+        private String Hoten;
+        private String ngaysinh;
+        private boolean gioitinh;
+        private String  email;
+        private String  avatar;
+        private String  TENTK;
+
+    public User() {
+    }
+
+    public String getMaND() {
+        return maND;
+    }
+
+    public void setMaND(String maND) {
+        this.maND = maND;
+    }
+
+    public String getHoten() {
+        return Hoten;
+    }
+
+    public void setHoten(String Hoten) {
+        this.Hoten = Hoten;
+    }
+
+    public String getNgaysinh() {
+        return ngaysinh;
+    }
+
+    public void setNgaysinh(String ngaysinh) {
+        this.ngaysinh = ngaysinh;
+    }
+
+    public boolean isGioitinh() {
+        return gioitinh;
+    }
+
+    public void setGioitinh(boolean gioitinh) {
+        this.gioitinh = gioitinh;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getTENTK() {
+        return TENTK;
+    }
+
+    public void setTENTK(String TENTK) {
+        this.TENTK = TENTK;
+    }
+
+    public User(String maND, String Hoten, String ngaysinh, boolean gioitinh, String email, String avatar, String TENTK) {
+        this.maND = maND;
+        this.Hoten = Hoten;
+        this.ngaysinh = ngaysinh;
+        this.gioitinh = gioitinh;
+        this.email = email;
+        this.avatar = avatar;
+        this.TENTK = TENTK;
+    }
+   
 }
